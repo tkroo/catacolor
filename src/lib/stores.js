594e025517e57b27d21e9d1aa336635b9ac1ed94
David Sullivan <311316+tkroo@users.noreply.github.com>
2025-07-28 07:54:29 +0000

    update gogh link

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -3,5 +3,5 @@ import { writable } from "svelte/store";
 export const formats = writable([
   {name: 'Cataclysm: DDA', url:'https://github.com/CleverRaven/Cataclysm-DDA/tree/master/data/raw/color_themes'},
   {name: 'Dwarf Fortress', url:'https://manmademagic.github.io/DFColorGen/'},
-  {name: 'Gogh', url:'https://github.com/Gogh-Co/Gogh/tree/master/json'}
+  {name: 'Gogh', url:'https://github.com/Gogh-Co/Gogh/tree/master/data/json'}
 ]);
